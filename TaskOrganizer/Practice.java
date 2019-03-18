aa8d06f179b5c453441177f116037a5e03e391b2
Chase Peak <chasepeak@Chases-MacBook-Pro.local>
2019-03-18 11:25:49 -0700

    user interface implemented

--- TaskOrganizer/Practice.java
+++
@@ -1,8 +0,0 @@
-import java.time.MonthDay;
-
-public class Practice {
-    public static void main(String[] args) {
-        MonthDay x = MonthDay.of(3,12);
-        System.out.println(x);
-    }
-}
